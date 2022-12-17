0e07bd6a1bdfd06eb5cb26d7280241fc610e35c0
Rhqo <rhqo123@gmail.com>
2022-12-17 17:51:32 +0900

    memo 없앰

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ChoKo-Memo' });
+  res.render('index', { title: 'ChoKo' });
 });
 
 module.exports = router;
