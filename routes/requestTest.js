510572f5766d8a8410a1a502a6f01080b435cb00
minseoi <minseoi.dev@gmail.com>
2022-12-18 18:00:38 +0900

    서버 api 통신 테스트

---
+++ routes/requestTest.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var request = require('request')
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  //쿼리로 loginId받아서, 로그인 api 찔러보기
+  console.log(`(/req) ID = ${req.query.loginId}`)
+  const options = {
+    url: '',
+    method: 'POST',
+    json: true,
+    body:{
+      loginId: req.query.loginId
+    }
+  }
+  request.post(options, function(err, resaponse, body){
+    console.log(`(/req) On Callback`)
+    if(err)
+      throw err
+    res.send(body)
+  })
+});
+
+module.exports = router;
