1b341e1beec1d1a335b11e601082892703e234ae
minseoi <minseoi.dev@gmail.com>
2023-01-26 23:36:27 +0900

    로그인 성공시 로컬스토리지에 토근저장

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -3,6 +3,7 @@ form[0].addEventListener('submit', (evt)=>{
     const accountId = document.getElementById('id').value;
     const password = document.getElementById('password').value;
 
+    
     fetch('http://server.chokospace.kro.kr:3901/api/chokomemo/signin', {
         credentials: 'omit',
         method: 'POST',
@@ -11,12 +12,26 @@ form[0].addEventListener('submit', (evt)=>{
     })
     .then(response => response.json())
     .then(data => {
-        alert(JSON.stringify(data))
+        response = JSON.parse(JSON.stringify(data))
+        if (err = response.error){
+            //나중에 에러코드로 관리 해야겠다.
+            if(err.message == "Invalid user"){
+                alert("계정정보가 일치하지 않습니다.")
+            }
+            return
+        }
+
+        //로컬 스토리지에 저장
+        localStorage.setItem("userId", response.userId)
+        localStorage.setItem("token", response.token)
+        
+        //메인페이지로 이동
+        window.location="/"
     })
     .catch(error => {
         alert(error)
         console.error(error);
     });
-    //창 넘어가지 않도록
+    //페이지 넘어가지 않도록
     evt.preventDefault();
 });
