63a00d6de82470ad50e9dea818f6d8cff31cb633
Rhqo <rhqo123@gmail.com>
2023-02-15 20:42:26 +0900

    signin 방식 변경 (login.js 생략)

--- routes/account.js
+++ routes/account.js
@@ -9,6 +9,40 @@ router.get('/signin', function(req, res, next) {
     res.render('login', { title:"Signin", showAccountOptions:false });
 });
 
+router.post('/signin', function(req, res, next) {
+  const accountId = req.body.floatingInput;
+  const password = req.body.floatingPassword;
+
+  fetch('http://server.chokospace.kro.kr:3901/api/chokomemo/signin', {
+        credentials: 'omit',
+        method: 'POST',
+        body: JSON.stringify({ accountId, password }),
+        headers: { 'Content-Type': 'application/json' }
+  })
+  .then(response => response.json())
+  .then(data => {
+      response = JSON.parse(JSON.stringify(data))
+      if (err = response.error){
+          //나중에 에러코드로 관리 해야겠다.
+          if(err.message == "Invalid user"){
+            res.write('<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>')
+            res.write('<script>alert("계정정보가 일치하지 않습니다.")</script>');
+            res.write("<script>window.location=\"/account/signin\"</script>");
+          }
+          return
+      }
+      
+      res.cookie('userId', `${response.userId}`, { path: '/'})
+      res.cookie('token', `${response.token}`, { path:'/' })
+
+      //메인페이지로 이동
+      res.redirect('/memo')
+  })
+  .catch(error => {
+      console.error(error);
+  });
+});
+
 router.get('/signout', function(req, res, next) {
   res.clearCookie("userId")
   res.clearCookie("token")
